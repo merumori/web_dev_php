d59006324be1cc61b09e5c2db652d06caa4381b8
merumori <143690709+merumori@users.noreply.github.com>
2023-08-31 13:07:43 +0530

    assigns an initial value
    Develop a PHP program that assigns an initial value to the variable $x, then reassigns it to a new
    value, and finally echoes the value.

--- script_6.php
+++ script_6.php
@@ -1,14 +1,21 @@
 <?php
-$x=6;
-$y=7;
-echo"before swap.</br>";
-echo"x= $x </br>";
-echo"y= $y </br>";
-$x = $X * $Y;
-$y = $X / $Y;
-$X = $x / $y;
-echo"after swap.</br>";
-echo"x=$x</br>";
-echo"y=$y</br>";
 
+	$x = 5;
+	$y = 7;
+	
+	 echo "Before Swap.</br>";
+	 echo "x = $x </br>";
+	 echo "y = $y </br>";
+
+	 
+	 $x = $x * $y;
+	 $y = $x / $y;
+	 $x = $x / $y;
+
+	 echo "After Swap.</br>";
+	 echo "x = $x </br>";
+	 echo "y = $y </br>";
+	
+
+	
 ?>
