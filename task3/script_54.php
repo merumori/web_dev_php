07b76413415ff2baf90044904562e199ff920864
Mori Meru <143690709+merumori@users.noreply.github.com>
2023-10-30 08:39:58 +0530

    Update script_54.php

--- task3/script_54.php
+++ task3/script_54.php
@@ -1,6 +1,6 @@
 <?php
 //// Script 54 extracts specific column from multidimensional array using the array_column() function.
-$array1= array(array("Name"=>"Mahesh","Age"=>21,"course"=>"MCA"),
-array("Name"=>"Daksh","Age"=>21,"course"=>"Msc"));
+$array1= array(array("Name"=>"Mori","Age"=>21,"course"=>"MCA"),
+array("Name"=>"mahes","Age"=>21,"course"=>"Msc"));
 print_r(array_column($array1,"Name"));
 ?>
