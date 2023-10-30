fd59f933bdaed7a5743361fefc407bea54089834
Mori Meru <143690709+merumori@users.noreply.github.com>
2023-10-30 08:38:00 +0530

    Update script_44.php

--- task3/script_44.php
+++ task3/script_44.php
@@ -1,7 +1,7 @@
 <?php
 //script 44 to extract values from associative array using array_values().
  
-$Name=array("Mca"=>"Mahesh","Mba"=>"Priyank","Msc"=>"Yatin","Bca"=>"Renil");
+$Name=array("Mca"=>"Mahesh","Mba"=>"mori","Msc"=>"Yatin","Bca"=>"Renil");
 
 $value=array_values($Name); 
 print_r($value);
